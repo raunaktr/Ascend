8627ea79f55739cb97b28dcf678a64859dd32b14
Raunak <reachtoraunak@gmail.com>
2021-11-17 11:22:32 +0530

    [Add] Navbar, hero and footer component

---
+++ components/Hero/Hero.js
@@ -0,0 +1,23 @@
+export default function Hero() {
+  return (
+    <>
+      <div className="hero min-h-screen">
+        <div className="relative px-4 xl-px-0 container mx-auto grid sm:grid-cols-1 md-grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+          <div className="text-color p-1">
+            <h1 className="text-6xl w-1/2 xl:w-full xl:text-9xl md:text-6xl sm:text-5xl font-black f-f-l">
+              Learn. <span className="text-primary">Analyze. </span>Apply.
+            </h1>
+            <div className="f-f-r text-xl lg:text-3xl pb-20 sm:pb-0 pt-10 xl:pt-20">
+              Accelerate your learning with state-of-art notes, easy to grasp, and learn with on on
+              the journey for UPSC Prelims, mains and Interview Preparation.
+            </div>
+            <button className="hover:opacity-90 text-xl w-full xl:text-2xl xl:w-10/12 mt-4 xl:mt-10 f-f-r py-5 bg-primary text-white font-bold">
+              Learn More
+            </button>
+          </div>
+          <div className="bg-blue-50"></div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import { useState } from "react";
+
+// import logo from "../assets/images/logo.svg";
+
+export default function Navbar() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <>
+      <nav className="navbar flex flex-wrap bg-base-200 shadow-lg mb-2 py-10 px-4">
+        <Link href="/">
+          <a className="inline-flex items-center px-2 mx-4 ">
+            <span className="text-xl font-bold">Ascend</span>
+          </a>
+        </Link>
+        <button
+          className=" inline-flex p-3 rounded lg:hidden ml-auto outline-none"
+          onClick={handleClick}>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div className={`${active ? "" : "hidden"} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
+          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-stretch flex flex-col lg:h-auto">
+            <Link href="/">
+              <a className="btn btn-ghost btn-sm rounded-btn">Home</a>
+            </Link>
+            <Link href="/about">
+              <a className="btn btn-ghost btn-sm rounded-btn">About</a>
+            </Link>
+            <Link href="/portfolio">
+              <a className="btn btn-ghost btn-sm rounded-btn">Portfolio</a>
+            </Link>
+            <Link href="/blog">
+              <a className="btn btn-ghost btn-sm rounded-btn">Blog</a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
